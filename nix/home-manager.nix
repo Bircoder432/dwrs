4e4504d073e24cf8ae5da739054ecedac5777187
Vadim Storozhilov <yanechearer@gmail.com>
2026-01-08 21:14:55 +0500

    fix home-manager modules

--- nix/home-manager.nix
+++ nix/home-manager.nix
@@ -7,9 +7,6 @@
 
 with lib;
 
-let
-  cfg = config.programs.dwrs;
-in
 {
   options.programs.dwrs = {
     enable = mkEnableOption "dwrs downloader";
@@ -19,29 +16,26 @@ in
       default = pkgs.dwrs;
       description = "dwrs package to use";
     };
-    settings = {
-      template = mkOption {
-        type = types.str;
-        default = "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({percent}%) {msg}";
-      };
-
-      bar_chars = mkOption {
-        type = types.str;
-        default = "█▌░";
-      };
-
-      workers = mkOption {
-        type = types.int;
-        default = 1;
-      };
+    settings.template = mkOption {
+      type = types.str;
+      default = "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({percent}%) {msg}";
+    };
+    settings.bar_chars = mkOption {
+      type = types.str;
+      default = "█▌░";
+    };
+    settings.workers = mkOption {
+      type = types.int;
+      default = 1;
     };
   };
-  config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
+
+  config = mkIf (config.programs.dwrs.enable) {
+    home.packages = [ config.programs.dwrs.package ];
     xdg.configFile."dwrs/config.toml".text = ''
-      template = "${cfg.settings.template}"
-      bar_chars = "${cfg.settings.bar_chars}"
-      workers = ${toString cfg.settings.workers}
+      template = "${config.programs.dwrs.settings.template}"
+      bar_chars = "${config.programs.dwrs.settings.bar_chars}"
+      workers = ${toString config.programs.dwrs.settings.workers}
     '';
   };
 }
